4c69961aa43e49806c12bb7dd1cbeb14e88ae498
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2023-08-19 01:32:03 +0530

    Add files via upload

---
+++ Selection2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    int Standard = 0;
+
+    printf("Primary School Portal\n");
+
+    printf("Enter your division : \n");
+    scanf("%d",&Standard);
+    
+    // If else if ladder
+    if(Standard == 1)
+    {
+        printf("Your exam is at 1 PM\n");
+    }
+    else if(Standard == 2)
+    {
+        printf("Your exam is at 2 PM\n");
+    }
+    else if(Standard == 3)
+    {
+        printf("Your exam is at 3 PM\n");
+    }
+    else if(Standard == 4)
+    {
+        printf("Your exam is at 4 PM\n");
+    }
+    else{
+        printf("Invaild Standard\n");
+    }
+
+    return 0;
+}
