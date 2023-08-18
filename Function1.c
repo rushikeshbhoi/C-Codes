66a6a12e4c4b9b2879456d375804622904b232ba
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2023-08-19 01:20:11 +0530

    Add files via upload

---
+++ Function1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+//Function Defination
+void Display()
+{
+    printf("Inside Display Function...\n");
+}
+
+int main()
+{
+    printf("Inside main Function...\n");
+    Display();   // Function Call
+
+    return 0;
+}
