ff4b540b9664231b581b35a65600a38141ca7b60
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2023-09-09 22:34:47 +0530

    Add files via upload

---
+++ Structure12.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+union Demo
+{
+    int i;  // 4
+    char ch;    // 1
+    float f;    // 4
+    double d;   // 8
+};
+
+int main()
+{
+    union Demo obj;
+
+    printf("Size of object is : %d\n",sizeof(obj));
+
+    obj.f = 90.99;
+    printf("%f\n",obj.f);
+
+    obj.i = 11;
+    printf("%d\n",obj.i);
+
+    return 0;
+}
