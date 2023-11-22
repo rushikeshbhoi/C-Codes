0b8fac3f424aa2355e218034b6dad0be951b2c6e
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2023-11-23 00:28:37 +0530

    Add files via upload

---
+++ Multi6.java
@@ -0,0 +1,26 @@
+class Demo extends Thread
+{
+    public void run()   //  Running State
+    {
+    }
+}
+
+class Multi6
+{
+    public static void main(String Arg[]) throws Exception
+    {
+        System.out.println("Current thread is : "+Thread.currentThread().getName());
+
+        Demo obj1 = new Demo(); // New State
+        Demo obj2 = new Demo();
+        
+        obj1.setPriority(8);
+        obj2.setPriority(10);
+        
+        obj1.start();   // Runnable State
+        obj2.start();
+       
+        System.out.println("Priority of obj1 is : "+obj1.getPriority());
+        System.out.println("Priority of obj2 is : "+obj2.getPriority());
+    }
+}
